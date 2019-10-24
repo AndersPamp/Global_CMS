67586a54958bf00bd6b1a534d42f39b13bcda852
Paula Hernandez <paula.frost@hotmail.com>
2019-10-24 11:25:52 +0200

    initial commits

---
+++ global_react/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input></input>
+        <span>
+          <input></input>
+        </span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
