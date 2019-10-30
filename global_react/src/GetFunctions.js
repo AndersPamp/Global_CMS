ee344fb9e331142093d2799fc3f1f115b78e662c
Alexander Olofsson <Phenomphenom@live.se>
2019-10-30 14:31:05 +0100

    AddedGetFunctions

---
+++ global_react/src/GetFunctions.js
@@ -0,0 +1,15 @@
+const GetCountry = async (country) => {
+    return await fetch(`http://localhost:16479/${country}/?AltTemplate=AsJson`).then(result => result.json());    
+}
+
+const GetOffice = async (country, office) => {
+        return await fetch(`http://localhost:16479/${country}/${office}/?AltTemplate=AsJson`)
+}
+
+const GetDepartment = async (country, office, department) => {
+    return await fetch(`http://localhost:16479/${country}/${office}/${department}/?AltTemplate=AsJson`)
+}
+
+const GetDepartment = async (country, office, department, employee) => {
+    return await fetch(`http://localhost:16479/${country}/${office}/${department}/${employee}/?AltTemplate=AsJson`)
+}
--- global_react/src/Test.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const getDataFromUmbraco = async () => {
-    let rawData = await fetch('http://localhost:16479/')
-}    
