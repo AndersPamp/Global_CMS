c2b348fcf4b5819650d2aeabb3ffaa5b892eaf36
Anders Pamp <anders_pamp@hotmail.com>
2019-10-31 10:32:27 +0100

    Hook up React to CMS

--- global_react/src/App.js
+++ global_react/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./App.css";
+import {
+  GetCountry,
+  GetOffice
+  // GetDepartment,
+  // GetEmployee
+} from "./GetFunctions";
 
 function App() {
+  GetCountry("Sweden").then(lo => console.log(lo));
+  GetOffice("Sweden", "Global-Total").then(foo => console.log(foo));
   return (
     <div className="App">
       <header className="App-header">
